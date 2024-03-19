6bba3116f455f06a6188736f6fe5eea4687cfdec
Bhavya Garg <bebhavyagarg@gmail.com>
2024-03-19 14:12:11 +0530

    not using create react app

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// // const App = () => {
+// //     return React.createElement(
+// //         "div",
+// //         {},
+// //         React.createElement("h1", {}, "Adopt Me!")
+// //     );
+// // };
+
+// // const container = document.getElementById("root");
+// // const root = ReactDOM.createRoot(container);
+// // root.render(React.createElement(App));
+
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement(h1, {}, props.name),
+//     React.createElement(h1, {}, props.animal),
+//     React.createElement(h1, {}, props.breed),
+//   ]);
+// };
+
+// const InitialApp = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement(Pet, {
+//       name: "Ammie",
+//       animal: "Dog",
+//       breed: "Husky",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Tom",
+//       animal: "Dog",
+//       breed: "German Shephard",
+//     }),
+//   ]);
+// };
+
+// const container = document.getElementById("root");
+// const root = ReactDOM.createRoot(container);
+// root.render(React.createElement(InitialApp));
---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import {React} from "react";
+import {ReactDOM} from "react-dom";
+
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, props.name),
+      React.createElement("h1", {}, props.animal),
+      React.createElement("h1", {}, props.breed),
+    ]);
+  };
+  
+  const InitialApp = () => {
+    return React.createElement("div", {}, [
+      React.createElement(Pet, {
+        name: "Ammie",
+        animal: "Dog",
+        breed: "Husky",
+      }),
+      React.createElement(Pet, {
+        name: "Tom",
+        animal: "Dog",
+        breed: "German Shephard",
+      }),
+    ]);
+  };
+  
+  const container = document.getElementById("root");
+  const root = ReactDOM.createRoot(container);
+  root.render(React.createElement(InitialApp));
+  
