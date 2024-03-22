6d6451971fb3cc1f3479fcc21f2f08bcecf521b7
Bhavya Garg <bebhavyagarg@gmail.com>
2024-03-22 15:37:13 +0530

    Used map() to render an array of components from a data file

--- src/important_concepts/customa_arguments_to_event_functions/src/App.jsx
+++ src/important_concepts/customa_arguments_to_event_functions/src/App.jsx
@@ -32,12 +32,15 @@ const App = () => {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title}
-              // title={conceptItem.title}
-              // description={conceptItem.description}
-              // image={conceptItem.image}
-              {...conceptItem}
-            />)}
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept
+                key={conceptItem.title}
+                // title={conceptItem.title}
+                // description={conceptItem.description}
+                // image={conceptItem.image}
+                {...conceptItem}
+              />
+            ))}
             {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
