d2adbf219db6e15895dac4a73791a2edecffd0ea
Bhavya Garg <bebhavyagarg@gmail.com>
2024-03-22 15:36:43 +0530

    Used map() to render an array of components from a data file

--- src/important_concepts/customa_arguments_to_event_functions/src/App.jsx
+++ src/important_concepts/customa_arguments_to_event_functions/src/App.jsx
@@ -1,9 +1,9 @@
-import {useState} from "react";
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import TabButton from "./TabButton";
 import "./index.css";
-import {CORE_CONCEPTS, EXAMPLES} from './data';
-import CoreConcept from "./CoreConcept"
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
+import CoreConcept from "./CoreConcept";
 
 const App = () => {
   // hook must be a top element of a function component
@@ -15,66 +15,84 @@ const App = () => {
 
   let tabContent = <p>Please select a button..</p>;
 
-  if(selectedTopic)
-{
-  tabContent = <div id="tab-content">
-
-  <h3>{EXAMPLES[selectedTopic].title}</h3>
-  <p>{EXAMPLES[selectedTopic].description}</p>
-  <pre>
-    <code>{EXAMPLES[selectedTopic].code}</code>
-  </pre>
-  </div>
-}
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
   return (
     <div>
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-          <CoreConcept title = {CORE_CONCEPTS[0].title}
-          description = {CORE_CONCEPTS[0].description}
-          image = {CORE_CONCEPTS[0].image}/>
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title}
+              // title={conceptItem.title}
+              // description={conceptItem.description}
+              // image={conceptItem.image}
+              {...conceptItem}
+            />)}
+            {/* <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
 
-<CoreConcept title = {CORE_CONCEPTS[1].title}
-          description = {CORE_CONCEPTS[1].description}
-          image = {CORE_CONCEPTS[1].image}/>
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
 
-<CoreConcept title = {CORE_CONCEPTS[2].title}
-          description = {CORE_CONCEPTS[2].description}
-          image = {CORE_CONCEPTS[2].image}/>
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+            />
 
-<CoreConcept title = {CORE_CONCEPTS[3].title}
-          description = {CORE_CONCEPTS[3].description}
-          image = {CORE_CONCEPTS[3].image}/>
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            /> */}
           </ul>
-          </section>
-          
+        </section>
+
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton isSelected={selectedTopic ==='components' ? true : false}
+            <TabButton
+              isSelected={selectedTopic === "components" ? true : false}
               onSelect={() => {
                 handleClick("components");
               }}
             >
               Component
             </TabButton>
-            <TabButton isSelected={selectedTopic ==='jsx' ? true : false}
+            <TabButton
+              isSelected={selectedTopic === "jsx" ? true : false}
               onSelect={() => {
                 handleClick("jsx");
               }}
             >
               JSX
             </TabButton>
-            <TabButton isSelected={selectedTopic ==='props' ? true : false}
+            <TabButton
+              isSelected={selectedTopic === "props" ? true : false}
               onSelect={() => {
                 handleClick("props");
               }}
             >
               Props
             </TabButton>
-            <TabButton isSelected={selectedTopic ==='state' ? true : false}
+            <TabButton
+              isSelected={selectedTopic === "state" ? true : false}
               onSelect={() => {
                 handleClick("state");
               }}
@@ -93,8 +111,8 @@ const App = () => {
 </pre>
 </div>} */}
 
-{/* Another way */}
-{/* {!selectedTopic && <p>Please select a button..</p> }
+          {/* Another way */}
+          {/* {!selectedTopic && <p>Please select a button..</p> }
 {selectedTopic && <div id="tab-content">
 
 <h3>{EXAMPLES[selectedTopic].title}</h3>
@@ -104,10 +122,8 @@ const App = () => {
 </pre>
 </div>} */}
 
-{/* Last way */}
-{tabContent}
-
-
+          {/* Last way */}
+          {tabContent}
         </section>
       </main>
     </div>
--- src/important_concepts/customa_arguments_to_event_functions/src/TabButton.jsx
+++ src/important_concepts/customa_arguments_to_event_functions/src/TabButton.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-const TabButton = ({children , onSelect, isSelected}) => {
+const TabButton = ({ children, onSelect, isSelected }) => {
   return (
-    <button className={isSelected ? "active" : undefined} id="button" onClick = {()=> onSelect()}>
+    <button
+      className={isSelected ? "active" : undefined}
+      id="button"
+      onClick={() => onSelect()}
+    >
       {children}
     </button>
   );
-}
+};
 
 export default TabButton;
