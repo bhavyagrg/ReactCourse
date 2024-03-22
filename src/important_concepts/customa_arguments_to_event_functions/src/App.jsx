46d73a0eb10e4606c11cef69f038a57ab32e8215
Bhavya Garg <bebhavyagarg@gmail.com>
2024-03-22 11:44:25 +0530

    Started states and hooks

--- src/important_concepts/customa_arguments_to_event_functions/src/App.jsx
+++ src/important_concepts/customa_arguments_to_event_functions/src/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import {useState} from "react";
 import { createRoot } from "react-dom/client";
 import TabButton from "./TabButton";
 import "./index.css";
+import {EXAMPLES} from './data';
 
 const App = () => {
-  let tabContent = "Please click a button..";
+  // hook must be a top element of a function component
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
   function handleClick(selectedBtn) {
-    console.log(selectedBtn);
+    setSelectedTopic(selectedBtn);
   }
 
   return (
@@ -18,34 +20,41 @@ const App = () => {
           <menu>
             <TabButton
               onSelect={() => {
-                handleClick("Component");
+                handleClick("components");
               }}
             >
               Component
             </TabButton>
             <TabButton
               onSelect={() => {
-                handleClick("JSX");
+                handleClick("jsx");
               }}
             >
               JSX
             </TabButton>
             <TabButton
               onSelect={() => {
-                handleClick("Props");
+                handleClick("props");
               }}
             >
               Props
             </TabButton>
             <TabButton
               onSelect={() => {
-                handleClick("State");
+                handleClick("state");
               }}
             >
               State
             </TabButton>
           </menu>
-          {tabContent}
+          {/* content */}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+            </div>
         </section>
       </main>
     </div>
